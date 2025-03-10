93ac30231dad436cf7cf16c95cf1e9a65b497a65
cihong.liu <cihong.liu@foxmail.com>
2025-03-10 08:39:00 +0800

    优化 createAxiosInstance 函数，直接解构 validateUser 返回值

--- src/create-axios-instance.ts
+++ src/create-axios-instance.ts
@@ -6,14 +6,13 @@ import { K3ConfigEntry } from './k3-config-entry';
 
 /** 创建 axios 实例 */
 export const createAxiosInstance = async (config: K3ConfigEntry) => {
-  const result = await validateUser(config);
+  const { LoginResultType, KDSVCSessionId } = await validateUser(config);
 
   return axios.create({
     baseURL: config.url,
     headers: {
       'Content-Type': 'application/json',
-      'kdservice-sessionid':
-        result.LoginResultType === 1 ? result.KDSVCSessionId : '',
+      'kdservice-sessionid': LoginResultType === 1 ? KDSVCSessionId : '',
     },
     withCredentials: true,
   });
