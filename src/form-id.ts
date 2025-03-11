027d71c05658182246a86d6918f85878a8e87e20
cihong.liu <cihong.liu@foxmail.com>
2025-03-11 14:02:18 +0800

    修改 package.json 中的安装脚本为 postinstall，并在 form-id.ts 中添加新的单据类型 PRD_PPBOM

--- src/form-id.ts
+++ src/form-id.ts
@@ -1,12 +1,13 @@
 /** 金蝶单据类型 */
-export type FormId = 'BD_MATERIAL'          // 物料 
-    | 'BOS_ASSISTANTDATA_DETAIL'            // 辅助属性
-    | 'PRD_INSTOCK'                         // 生产入库单
-    | 'PRD_MO'                              // 生产订单
-    | 'STK_AssembledApp'                    // 组装拆卸单
-    | 'STK_MISCELLANEOUS'                   // 其他入库单
-    | 'STK_MisDelivery'                     // 其他出库单
-    | 'STK_LOTADJUST'                       // 批号调整单
-    | 'STK_StatusConvert'                   // 形态转换单
-    | 'STK_TransferDirect'                  // 直接调拨单
-    | 'IOS_PriceList';                      // 组织间结算价目表
+export type FormId = 'BD_MATERIAL'  // 物料 
+    | 'BOS_ASSISTANTDATA_DETAIL'    // 辅助属性
+    | 'PRD_INSTOCK'                 // 生产入库单
+    | 'PRD_MO'                      // 生产订单
+    | 'PRD_PPBOM'                   // 生产用料清单
+    | 'STK_AssembledApp'            // 组装拆卸单
+    | 'STK_MISCELLANEOUS'           // 其他入库单
+    | 'STK_MisDelivery'             // 其他出库单
+    | 'STK_LOTADJUST'               // 批号调整单
+    | 'STK_StatusConvert'           // 形态转换单
+    | 'STK_TransferDirect'          // 直接调拨单
+    | 'IOS_PriceList';              // 组织间结算价目表
