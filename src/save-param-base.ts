06112603d5c9c6ea20757c98ac314a82483a9ce4
cihong.liu <cihong.liu@foxmail.com>
2025-03-12 16:04:28 +0800

    修正 SaveParamBase 类中 NeedUpdateFields 字段的拼写错误

--- src/save-param-base.ts
+++ src/save-param-base.ts
@@ -5,7 +5,7 @@ export abstract class SaveParamBase {
   /** 需返回结果的字段集合，数组类型，格式：[key,entitykey.key,...]（非必录） 注（返回单据体字段格式：entitykey.key） */
   NeedReturnFields?: string[];
   /** 需要更新的字段，数组类型，格式：[key1,key2,...] （非必录）注（更新字段时Model数据包中必须设置内码，若更新单据体字段还需设置分录内码） */
-  NeedUpdateFields?: string[];
+  NeedUpDateFields?: string[];
   /** 是否删除已存在的分录，布尔类型，默认true（非必录） */
   IsDeleteEntry?: boolean;
   /** 是否验证所有的基础资料有效性，布尔类，默认false（非必录） */
