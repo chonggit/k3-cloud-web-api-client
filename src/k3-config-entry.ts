2ac8b5a3b77634f26176400c7b153dbedb4493ac
cihong.liu <cihong.liu@foxmail.com>
2025-03-14 15:16:11 +0800

    更新 K3ConfigEntry 类型，添加 API 地址、数据中心ID、用户名、应用Id 和应用秘钥字段，并将密码字段标记为已弃用

--- src/k3-config-entry.ts
+++ src/k3-config-entry.ts
@@ -2,9 +2,21 @@
 
 /** 金蝶配置 */
 export type K3ConfigEntry = {
+  /** 金蝶 API 地址 */
   url: string;
+  /** 数据中心ID */
   acctid: string;
+  /** 用户名 */
   username: string;
-  password: string;
+  /**
+   * 登录密码
+   * @deprecated 使用加密登录，不明传输密码
+   */
+  password?: string;
+  /** 语言标识符 */
   lcid?: number;
+  /** 第三方系统应用Id */
+  appId?: string;
+  /** 第三方系统应用秘钥 */
+  appSecret?: string;
 };
