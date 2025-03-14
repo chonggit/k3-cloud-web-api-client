b9814f96bfb9ac788a1cf778931e3d077f9b4c15
cihong.liu <cihong.liu@foxmail.com>
2025-03-14 15:23:41 +0800

    标记 validateUser 函数为已弃用，建议使用 loginBySign 函数替代

--- src/validate-user.ts
+++ src/validate-user.ts
@@ -4,9 +4,11 @@ import axios from 'axios';
 import { K3ConfigEntry } from './k3-config-entry';
 import { VALIDATE_USER_SERVICE_NAME } from './k3-cloud-services';
 import { ValidateUserResult } from './validate-user-result';
+import type { loginBySign } from './login-by-sign';
 
 /**
  * 用户登录
+ * @deprecated 使用 {@link loginBySign loginBySign}
  */
 export const validateUser = async (config: K3ConfigEntry) => {
   const requestData = {
