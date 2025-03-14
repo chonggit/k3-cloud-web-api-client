850cfcbded70c052706fbf99502e517aa772801e
cihong.liu <cihong.liu@foxmail.com>
2025-03-14 15:23:48 +0800

    重构 createAxiosInstance 函数，新增 getValidateUserResult 方法以支持基于签名的登录功能

--- src/create-axios-instance.ts
+++ src/create-axios-instance.ts
@@ -2,11 +2,21 @@
 
 import axios from 'axios';
 import { validateUser } from './validate-user';
+import { loginBySign } from './login-by-sign';
 import { K3ConfigEntry } from './k3-config-entry';
+
+function getValidateUserResult(config: K3ConfigEntry) {
+  if (config.appId) {
+    return loginBySign(config);
+  }
+  return validateUser(config);
+}
 
 /** 创建 axios 实例 */
 export const createAxiosInstance = async (config: K3ConfigEntry) => {
-  const { LoginResultType, KDSVCSessionId } = await validateUser(config);
+  const { LoginResultType, KDSVCSessionId } = await getValidateUserResult(
+    config
+  );
 
   return axios.create({
     baseURL: config.url,
